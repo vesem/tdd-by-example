2e2df5fb8a87f79fa5c2183d2d23b5e1a8869cb3
vesem <mitchellthom@gmail.com>
2020-08-10 18:46:21 -0400

    Refactor into Dollars and Francs Money class further

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -17,8 +17,8 @@ public class MoneyTest {
     @Test
     void testEqualityDollar() {
         assertEquals(Money.dollar(5), Money.dollar(5));
-        assertNotEquals(Money.dollar(5), new Dollar(8));
-        assertNotEquals(Money.dollar(5), new Franc(5));
+        assertNotEquals(Money.dollar(5), Money.dollar(8));
+        assertNotEquals(Money.dollar(5), Money.franc(5));
     }
 
     @Test
@@ -30,7 +30,13 @@ public class MoneyTest {
 
     @Test
     void testEqualityFranc() {
-        assertEquals(Money.franc(5), new Franc(5));
-        assertNotEquals(Money.franc(5), new Franc(8));
+        assertEquals(Money.franc(5), Money.franc(5));
+        assertNotEquals(Money.franc(5), Money.franc(8));
+    }
+
+    @Test
+    void testCurrencty() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
     }
 }
