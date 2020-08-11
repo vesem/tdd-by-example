532966ee321bb1f21918e379c8dce4c9e8e737fe
vesem <mitchellthom@gmail.com>
2020-08-11 16:53:29 -0400

    Add expression and bank classes

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,6 +1,6 @@
 package guru.springframework;
 
-public class Money {
+public class Money implements Expression{
 
     protected int amount;
     protected String currency;
@@ -16,6 +16,10 @@ public class Money {
 
     public Money times(int multiplier) {
         return new Money(amount * multiplier, this.currency);
+    }
+
+    public Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 
     public static Money dollar(int amount) {
